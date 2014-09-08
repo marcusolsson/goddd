55b786c63e4e1f28f10c79f0046c71356d1f3aae
Marcus Olsson <ninjasinventedphysics@gmail.com>
2014-09-08 22:56:26 +0200

    Fix minor code formatting.

--- routing/routing.go
+++ routing/routing.go
@@ -9,8 +9,7 @@ type RoutingService interface {
 // Our end of the routing service. This is basically a data model
 // translation layer between our domain model and the API put forward
 // by the routing team, which operates in a different context from us.
-type externalRoutingService struct {
-}
+type externalRoutingService struct{}
 
 func (s *externalRoutingService) FetchRoutesForSpecification(routeSpecification cargo.RouteSpecification) []cargo.Itinerary {
 	// TODO: Port pathfinder
