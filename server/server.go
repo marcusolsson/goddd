97aa0d78388a219f09be150aff25705185323b34
Marcus Olsson <ninjasinventedphysics@gmail.com>
2014-09-20 12:31:23 +0200

    Add simple Martini REST API

---
+++ server/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+)
+
+type Cargo struct {
+	TrackingId           string `json:"trackingId"`
+	StatusText           string `json:"statusText"`
+	Destination          string `json:"destination"`
+	ETA                  string `json:"eta"`
+	NextExpectedActivity string `json:"nextExpectedActivity"`
+}
+
+func main() {
+	m := martini.Classic()
+
+	m.Use(render.Renderer(render.Options{
+		IndentJSON: true,
+	}))
+
+	m.Get("/cargos", func(r render.Render) {
+		c := Cargo{
+			TrackingId:           "ABC123",
+			StatusText:           "In port New York",
+			Destination:          "Helsinki",
+			ETA:                  "2009-03-12 12:00",
+			NextExpectedActivity: "Next expected activity is to load cargo onto voyage 0200T in New York",
+		}
+
+		r.JSON(200, c)
+	})
+
+	m.Run()
+}
