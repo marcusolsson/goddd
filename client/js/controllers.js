cc232e55ef9436fe460c15e8fbf2cdaf3414df63
Marcus Olsson <ninjasinventedphysics@gmail.com>
2014-09-20 10:42:43 +0200

    Extracted cargo object from template.

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,6 +1,16 @@
 var trackApp = angular.module('trackApp', []);
 
 trackApp.controller('TrackCtrl', function ($scope) {
+
+    $scope.cargo = {
+	'trackingId': 'ABC123',
+	'transportStatus': 'In port',
+	'location': 'New York',
+	'destination': 'Helsinki',
+	'estimatedArrival': '2009-03-12 12:00',
+	'nextActivity': 'load cargo onto voyage 0200T in New York'
+    }
+
     $scope.events = [
 	{'text' : 'Received in Hongkong, at 3/1/09 12:00 AM.'},
 	{'text' : 'Loaded onto voyage 0100S in Hongkong, at 3/2/09 12:00 AM.'},
