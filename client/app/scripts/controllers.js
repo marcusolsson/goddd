b4c4ae7716c1d46d576a1f9bf24985d328410685
Marcus Olsson <ninjasinventedphysics@gmail.com>
2014-09-24 21:51:50 +0200

    Use controls in book cargo modal for POST params.

--- client/app/scripts/controllers.js
+++ client/app/scripts/controllers.js
@@ -8,7 +8,7 @@ trackApp.factory("Cargo", function($resource) {
     return $resource("/cargos/:id", null, {
 	'find': {method: 'GET', params: {id: "@id"}},
 	'list': {method: 'GET', isArray: true},
-	'book': {method: 'POST', params: {origin: "AUMEL", destination: "SESTO", arrivalDeadline: 123}}
+	'book': {method: 'POST', params: {origin: "@origin", destination: "@destination", arrivalDeadline: "@arrivalDeadline"}}
     });
 });
 
@@ -33,10 +33,26 @@ trackApp.controller('ListCtrl', function ($scope, Cargo) {
 trackApp.controller('BookCargoCtrl', function ($scope, Location, Cargo) {
     Location.query(function(data) {
 	$scope.locations = data;
+	$scope.selectedOrigin = $scope.locations[0].locode
+	$scope.selectedDestination = $scope.locations[0].locode
     });
 
+    $scope.selectOrigin = function (locode) {
+	$scope.selectedOrigin = locode;
+    }
+
+    $scope.selectDestination = function (locode) {
+	$scope.selectedDestination = locode;
+    }
+
     $scope.bookCargo = function () {
-	Cargo.book(function(data) {
+	var deadlineDate = new Date($scope.deadline).getTime();
+
+	Cargo.book({
+	    origin: $scope.selectedOrigin,
+	    destination: $scope.selectedDestination,
+	    arrivalDeadline: deadlineDate
+	}, function(data) {
 	    $scope.bookedCargo = data
 	})
     }
