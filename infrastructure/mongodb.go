70b5c0ec1e90a64b12633c5a348ec3538988c42c
Marcus Olsson <ninjasinventedphysics@gmail.com>
2014-10-14 21:39:08 +0200

    Do a upsert (instead of insert) when storing a location

--- infrastructure/mongodb.go
+++ infrastructure/mongodb.go
@@ -21,7 +21,7 @@ func (r *locationRepositoryMongoDB) Store(l location.Location) {
 	defer session.Close()
 
 	c := session.DB("app30695645").C("locations")
-	err = c.UpsertId(l.UNLocode, &l)
+	_, err = c.UpsertId(l.UNLocode, &l)
 
 	if err != nil {
 		log.Fatal(err)
