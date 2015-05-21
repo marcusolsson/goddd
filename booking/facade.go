15d84ab2feca1a7cf9e6b6e420f257bf903d3a27
Marcus Olsson <ninjasinventedphysics@gmail.com>
2015-05-21 22:30:00 +0200

    Major refactor to remove bloated Java heritage

--- booking/facade.go
+++ booking/facade.go
@@ -2,16 +2,13 @@ package booking
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/marcusolsson/goddd/cargo"
-	"github.com/marcusolsson/goddd/location"
-	"github.com/marcusolsson/goddd/voyage"
 )
 
-type cargoDTO struct {
+type CargoDTO struct {
 	TrackingID           string     `json:"trackingId"`
 	StatusText           string     `json:"statusText"`
 	Origin               string     `json:"origin"`
@@ -22,19 +19,19 @@ type cargoDTO struct {
 	Routed               bool       `json:"routed"`
 	ArrivalDeadline      time.Time  `json:"arrivalDeadline"`
 	Events               []eventDTO `json:"events"`
-	Legs                 []legDTO   `json:"legs,omitempty"`
+	Legs                 []LegDTO   `json:"legs,omitempty"`
 }
 
-type locationDTO struct {
+type LocationDTO struct {
 	UNLocode string `json:"locode"`
 	Name     string `json:"name"`
 }
 
 type RouteCandidateDTO struct {
-	Legs []legDTO `json:"legs"`
+	Legs []LegDTO `json:"legs"`
 }
 
-type legDTO struct {
+type LegDTO struct {
 	VoyageNumber string    `json:"voyageNumber"`
 	From         string    `json:"from"`
 	To           string    `json:"to"`
@@ -47,111 +44,8 @@ type eventDTO struct {
 	Expected    bool   `json:"expected"`
 }
 
-type Facade interface {
-	BookNewCargo(origin, destination string, arrivalDeadline string) (string, error)
-	LoadCargoForRouting(trackingID string) (cargoDTO, error)
-	AssignCargoToRoute(trackingID string, candidate RouteCandidateDTO) error
-	ChangeDestination(trackingID string, destinationUNLocode string) error
-	RequestRoutesForCargo(trackingID string) []RouteCandidateDTO
-	ListShippingLocations() []locationDTO
-	ListAllCargos() []cargoDTO
-}
-
-type facade struct {
-	cargoRepository         cargo.Repository
-	locationRepository      location.Repository
-	handlingEventRepository cargo.HandlingEventRepository
-	Service                 Service
-}
-
-func (f *facade) BookNewCargo(origin, destination string, arrivalDeadline string) (string, error) {
-	millis, _ := strconv.ParseInt(fmt.Sprintf("%s", arrivalDeadline), 10, 64)
-	trackingID, err := f.Service.BookNewCargo(location.UNLocode(origin), location.UNLocode(destination), time.Unix(millis/1000, 0))
-
-	return string(trackingID), err
-}
-
-func (f *facade) LoadCargoForRouting(trackingID string) (cargoDTO, error) {
-	c, err := f.cargoRepository.Find(cargo.TrackingID(trackingID))
-
-	if err != nil {
-		return cargoDTO{}, err
-	}
-
-	return assemble(c, f.handlingEventRepository), nil
-}
-
-func (f *facade) AssignCargoToRoute(trackingID string, candidate RouteCandidateDTO) error {
-	var legs []cargo.Leg
-	for _, l := range candidate.Legs {
-		legs = append(legs, cargo.Leg{
-			VoyageNumber:   voyage.Number(l.VoyageNumber),
-			LoadLocation:   location.UNLocode(l.From),
-			UnloadLocation: location.UNLocode(l.To),
-			LoadTime:       l.LoadTime,
-			UnloadTime:     l.UnloadTime,
-		})
-	}
-
-	return f.Service.AssignCargoToRoute(cargo.Itinerary{Legs: legs}, cargo.TrackingID(trackingID))
-}
-
-func (f *facade) ChangeDestination(trackingID string, destinationUNLocode string) error {
-	return f.Service.ChangeDestination(cargo.TrackingID(trackingID), location.UNLocode(destinationUNLocode))
-}
-
-func (f *facade) RequestRoutesForCargo(trackingID string) []RouteCandidateDTO {
-	itineraries := f.Service.RequestPossibleRoutesForCargo(cargo.TrackingID(trackingID))
-
-	var candidates []RouteCandidateDTO
-	for _, itin := range itineraries {
-		var legs []legDTO
-		for _, leg := range itin.Legs {
-			legs = append(legs, legDTO{
-				VoyageNumber: string(leg.VoyageNumber),
-				From:         string(leg.LoadLocation),
-				To:           string(leg.UnloadLocation),
-				LoadTime:     leg.LoadTime,
-				UnloadTime:   leg.UnloadTime,
-			})
-		}
-		candidates = append(candidates, RouteCandidateDTO{Legs: legs})
-	}
-
-	return candidates
-}
-
-func (f *facade) ListShippingLocations() []locationDTO {
-	locations := f.locationRepository.FindAll()
-
-	dtos := make([]locationDTO, len(locations))
-	for i, loc := range locations {
-		dtos[i] = locationDTO{
-			UNLocode: string(loc.UNLocode),
-			Name:     loc.Name,
-		}
-	}
-
-	return dtos
-}
-
-func (f *facade) ListAllCargos() []cargoDTO {
-	cargos := f.cargoRepository.FindAll()
-	dtos := make([]cargoDTO, len(cargos))
-
-	for i, c := range cargos {
-		dtos[i] = assemble(c, f.handlingEventRepository)
-	}
-
-	return dtos
-}
-
-func NewFacade(s Service) Facade {
-	return &facade{Service: s}
-}
-
-func assemble(c cargo.Cargo, her cargo.HandlingEventRepository) cargoDTO {
-	return cargoDTO{
+func Assemble(c cargo.Cargo, her cargo.HandlingEventRepository) CargoDTO {
+	return CargoDTO{
 		TrackingID:           string(c.TrackingID),
 		Origin:               string(c.Origin),
 		Destination:          string(c.RouteSpecification.Destination),
@@ -197,10 +91,10 @@ func assembleStatusText(c cargo.Cargo) string {
 	}
 }
 
-func assembleLegs(c cargo.Cargo) []legDTO {
-	var legs []legDTO
+func assembleLegs(c cargo.Cargo) []LegDTO {
+	var legs []LegDTO
 	for _, l := range c.Itinerary.Legs {
-		legs = append(legs, legDTO{
+		legs = append(legs, LegDTO{
 			VoyageNumber: string(l.VoyageNumber),
 			From:         string(l.LoadLocation),
 			To:           string(l.UnloadLocation),
--- main.go
+++ main.go
@@ -1,20 +1,346 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"os"
 	"strconv"
+	"time"
 
-	"github.com/marcusolsson/goddd/server"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/hariharan-uno/cors"
+	"github.com/marcusolsson/goddd/booking"
+	"github.com/marcusolsson/goddd/cargo"
+	"github.com/marcusolsson/goddd/handling"
+	"github.com/marcusolsson/goddd/location"
+	"github.com/marcusolsson/goddd/repository"
+	"github.com/marcusolsson/goddd/routing"
+	"github.com/marcusolsson/goddd/voyage"
 )
 
-var port int
+var (
+	cargoRepository            = repository.NewCargo()
+	locationRepository         = repository.NewLocation()
+	voyageRepository           = repository.NewVoyage()
+	handlingEventRepository    = repository.NewHandlingEvent()
+	routingService             = routing.NewService("http://ddd-pathfinder.herokuapp.com")
+	bookingService             = booking.NewService(cargoRepository, locationRepository, routingService)
+	handlingEventServiceFacade = handling.NewFacade(cargoRepository, locationRepository, voyageRepository, handlingEventRepository)
+)
+
+var (
+	defaultPort = "8080"
+)
 
 func main() {
-	flag.IntVar(&port, "port", 8080, "the server port")
-	flag.Parse()
+	storeTestData(cargoRepository)
 
-	server.RegisterHandlers()
+	r := mux.NewRouter()
 
-	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	r.HandleFunc("/cargos", cargosHandler).Methods("GET")
+	r.HandleFunc("/cargos", bookCargoHandler).Methods("POST")
+	r.HandleFunc("/cargos/{id}", cargoHandler).Methods("GET")
+	r.HandleFunc("/cargos/{id}/request_routes", requestRoutesHandler).Methods("GET")
+	r.HandleFunc("/cargos/{id}/assign_to_route", assignToRouteHandler).Methods("POST")
+	r.HandleFunc("/cargos/{id}/change_destination", changeDestinationHandler).Methods("POST")
+	r.HandleFunc("/locations", locationsHandler).Methods("GET")
+	r.HandleFunc("/incidents", registerIncidentHandler).Methods("POST")
+
+	n := negroni.Classic()
+
+	options := cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type"},
+	}
+
+	n.Use(negroni.HandlerFunc(options.Allow))
+	n.UseHandler(r)
+
+	http.Handle("/", n)
+
+	http.ListenAndServe(":"+port(), nil)
+}
+
+func port() string {
+	p := os.Getenv("PORT")
+	if p == "" {
+		return defaultPort
+	}
+	return p
+}
+
+func cargosHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	cargos := cargoRepository.FindAll()
+
+	dtos := make([]booking.CargoDTO, len(cargos))
+	for i, c := range cargos {
+		dtos[i] = booking.Assemble(c, handlingEventRepository)
+	}
+
+	b, err := json.Marshal(dtos)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(b)
+}
+
+func cargoHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	vars := mux.Vars(req)
+
+	c, err := cargoRepository.Find(cargo.TrackingID(vars["id"]))
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	b, err := json.Marshal(booking.Assemble(c, handlingEventRepository))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(b)
+}
+
+func requestRoutesHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	vars := mux.Vars(req)
+
+	itineraries := bookingService.RequestPossibleRoutesForCargo(cargo.TrackingID(vars["id"]))
+
+	var candidates []booking.RouteCandidateDTO
+	for _, itin := range itineraries {
+		var legs []booking.LegDTO
+		for _, leg := range itin.Legs {
+			legs = append(legs, booking.LegDTO{
+				VoyageNumber: string(leg.VoyageNumber),
+				From:         string(leg.LoadLocation),
+				To:           string(leg.UnloadLocation),
+				LoadTime:     leg.LoadTime,
+				UnloadTime:   leg.UnloadTime,
+			})
+		}
+		candidates = append(candidates, booking.RouteCandidateDTO{Legs: legs})
+	}
+
+	b, err := json.Marshal(candidates)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(b)
+}
+
+func assignToRouteHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	b, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	defer req.Body.Close()
+
+	var candidate booking.RouteCandidateDTO
+	if err := json.Unmarshal(b, &candidate); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var legs []cargo.Leg
+	for _, l := range candidate.Legs {
+		legs = append(legs, cargo.Leg{
+			VoyageNumber:   voyage.Number(l.VoyageNumber),
+			LoadLocation:   location.UNLocode(l.From),
+			UnloadLocation: location.UNLocode(l.To),
+			LoadTime:       l.LoadTime,
+			UnloadTime:     l.UnloadTime,
+		})
+	}
+
+	vars := mux.Vars(req)
+
+	if err := bookingService.AssignCargoToRoute(cargo.Itinerary{Legs: legs}, cargo.TrackingID(vars["id"])); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func changeDestinationHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	v := queryParams(req.URL.Query())
+
+	found, missing := v.validateQueryParams("destination")
+
+	if len(missing) > 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	vars := mux.Vars(req)
+
+	if err := bookingService.ChangeDestination(cargo.TrackingID(vars["id"]), location.UNLocode(found["destination"].(string))); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
+func bookCargoHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	v := queryParams(req.URL.Query())
+
+	found, missing := v.validateQueryParams("origin", "destination", "arrivalDeadline")
+
+	if len(missing) > 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var (
+		origin          = found["origin"].(string)
+		destination     = found["destination"].(string)
+		arrivalDeadline = found["arrivalDeadline"].(string)
+	)
+
+	millis, _ := strconv.ParseInt(arrivalDeadline, 10, 64)
+	trackingID, err := bookingService.BookNewCargo(location.UNLocode(origin), location.UNLocode(destination), time.Unix(millis/1000, 0))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	c, err := cargoRepository.Find(trackingID)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	dto := booking.Assemble(c, handlingEventRepository)
+
+	b, err := json.Marshal(dto)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(b)
+}
+
+func locationsHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	locations := locationRepository.FindAll()
+
+	dtos := make([]booking.LocationDTO, len(locations))
+	for i, loc := range locations {
+		dtos[i] = booking.LocationDTO{
+			UNLocode: string(loc.UNLocode),
+			Name:     loc.Name,
+		}
+	}
+
+	b, err := json.Marshal(dtos)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(b)
+}
+
+func registerIncidentHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	v := queryParams(req.URL.Query())
+
+	found, missing := v.validateQueryParams("completionTime", "trackingId", "voyage", "location", "eventType")
+
+	if len(missing) > 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var (
+		completionTime = found["completionTime"].(string)
+		trackingID     = found["trackingId"].(string)
+		voyageNumber   = found["voyage"].(string)
+		location       = found["location"].(string)
+		eventType      = found["eventType"].(string)
+	)
+
+	if err := handlingEventServiceFacade.RegisterHandlingEvent(completionTime, trackingID, voyageNumber, location, eventType); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
+func storeTestData(r cargo.Repository) {
+	test1 := cargo.New("FTL456", cargo.RouteSpecification{
+		Origin:      location.AUMEL,
+		Destination: location.SESTO,
+	})
+	r.Store(*test1)
+
+	test2 := cargo.New("ABC123", cargo.RouteSpecification{
+		Origin:      location.SESTO,
+		Destination: location.CNHKG,
+	})
+	r.Store(*test2)
+}
+
+type queryParams url.Values
+
+type jsonObject map[string]interface{}
+
+func (p queryParams) validateQueryParams(params ...string) (found jsonObject, missing []string) {
+	found = make(jsonObject)
+	missing = make([]string, 0)
+
+	for _, param := range params {
+		s := url.Values(p).Get(param)
+		if len(s) > 0 {
+			found[param] = s
+		} else {
+			missing = append(missing, param)
+		}
+	}
+	return found, missing
 }
